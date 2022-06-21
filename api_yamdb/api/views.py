177ad0cc4a37e2b84e6ca283bec828451a791ded
Rustam A <69015050+lauman302@users.noreply.github.com>
2022-06-21 23:13:59 +0300

    fix

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -12,6 +12,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
 from reviews.models import Category, Genre, Review, Title
 
 from .filters import TitleFilter
