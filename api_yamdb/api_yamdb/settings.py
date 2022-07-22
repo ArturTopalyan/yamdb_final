2154c6b645aa04ffc85df070eba5a8a61b17dbcc
Артур Топалян <topabro@yandex.ru>
2022-07-22 10:58:44 +0300

    pytest check rdy flake8 too

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,17 +1,16 @@
-import os
 from datetime import timedelta
+import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='3$wqbf&14i^xgolnp!l%7m*pkfntaxhll9r)y$xhpp1c7__&by')
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs"
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '51.250.19.76', 'localhost']
+ALLOWED_HOSTS = ["*", "localhost"]
 
 
 # Application definition
